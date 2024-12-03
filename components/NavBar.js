7e771c33b14ab1e527b5ea96459c08b3648548fa
AlonsoDOK <alonso.dok@gmail.com>
2024-12-02 21:38:26 -0300

    primera entrega version cuatro

--- components/NavBar.js
+++ components/NavBar.js
@@ -4,7 +4,7 @@ const NavBar = () => {
             <div className="navbar-logo">
                 <img src="pato.png" alt="Logo de Compra tu Pato" />
             </div>
-            <h1>Compra tu Pato</h1>
+            git<h1>Compra tu Pato</h1>
             <div>
                 <a href="#inicio">Inicio</a>
                 <a href="#productos">Productos</a>
